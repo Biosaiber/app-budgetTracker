5548237ca9899c99a984b34861625a3fdf186765
isubfox <samuel.liska@gmail.com>
2026-05-01 22:59:42 +0200

    feat: load and display budget entries using RxJS observable

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { from, Subscription } from 'rxjs';
+import { delay } from 'rxjs/operators'
 import { BudgetEntry } from './models/budget-entry.interface';
 
+const budgetEntriesData: BudgetEntry[] = [
+  { id: 1, description: "Groceries", amount: 420 },
+  { id: 2, description: "Rent", amount: 690 },
+  { id: 3, description: "Kids", amount: 500 },
+  { id: 4, description: "Phone", amount: 32 },
+  { id: 5, description: "Gym", amount: 45 }
+]
 
 @Component({
   selector: 'app-root',
-
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+
+export class App implements OnInit, OnDestroy {
   budgetEntries: BudgetEntry[] = [];
+  budgetSubscription?: Subscription;
+
+  ngOnInit() {
+    this.budgetSubscription = this.simulateDataRetrieval().subscribe({
+      next: (data) => this.budgetEntries.push(data)
+    })
+  }
+  ngOnDestroy() {
+    this.budgetSubscription?.unsubscribe();
+  }
+
+  simulateDataRetrieval() {
+    return from(budgetEntriesData).pipe(delay(2000))
+  }
+    
 }
